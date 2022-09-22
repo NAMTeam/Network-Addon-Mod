8bf3ef490c3088fa5405963f4c96093ac0ec1559
jflann <j.flann1@gmail.com>
2022-09-22 07:23:20 -0500

    RRW resolver & rule generator prototype updates

--- src/main/scala/module/RealRailwayResolver.scala
+++ src/main/scala/module/RealRailwayResolver.scala
@@ -15,8 +15,13 @@ class RealRailwayResolver extends IdResolver {
       }
     }
     // ortho
+    add(Rail~NS, 0x03031500)
     add(L1Dtr~NS, 0x5d640000)
-    add(L2Dtr~NS, 0x5d540000)
+    add(L2Dtr~NS, 0x5d740000)
+    add(Str~NS, 0x5d300000)
+    // diag
+    // add(L1Dtr~)
+    // add(L2Dtr~)
     /* DTR Crossings (L0, L1, & L2)
     OxO
     OxD
@@ -30,39 +35,63 @@ class RealRailwayResolver extends IdResolver {
     */
     // ----- OxO -----
     // -- Street --
-    add(Street~NS & Rail~WE, 0x05010100)
+    add(Street~NS & Rail~WE,  0x05010100)
     add(Street~NS & L1Dtr~WE, 0x5d671000)
     add(Street~NS & L2Dtr~WE, 0x5d771000)
-    add(Street~NS & Str~WE, 0x5d340000)
+    add(Street~NS & Str~WE,   0x5d340000)
     // -- Road --
-    add(Road~NS & Rail~WE, 0x03010100)
-    add(Road~NS & L1Dtr~WE, 0x5d671100)
-    add(Road~NS & L2Dtr~WE, 0x5d771100)
-    add(Road~NS & Str~WE, 0x5d341000)
+    add(Road~NS & Rail~WE,  0x03010100)
+    add(Road~WE & L1Dtr~NS, 0x5d671100)
+    add(Road~WE & L2Dtr~NS, 0x5d771100)
+    add(Road~NS & Str~WE,   0x5d341000)
     // -- Road L1 --
-    add(L1Road~NS & Rail~WE, 0x5c001500)
-    add(L1Road~NS & L2Dtr~WE, 0x5d771110)
-    add(L1Road~NS & Str~WE, 0x5c001505)
+    add(L1Road~NS & Rail~WE,  0x5c001500)
+    add(L1Road~NS & L2Dtr~WE, 0x5d771105)
+    add(L1Road~NS & Str~WE,   0x5c001505)
     // -- Road L2 --
-    add(L2Road~NS & Rail~WE, 0x5c031500)
-    add(L2Road~NS & L1Dtr~WE, 0x5d671120)
-    add(L2Road~NS & Str~WE, 0x5c031505)
+    add(L2Road~NS & Rail~WE,  0x5c031500)
+    add(L2Road~WE & L1Dtr~NS, 0x5d67110a)
+    add(L2Road~NS & Str~WE,   0x5c031505)
     // -- OWR --
-    add(Onewayroad~NS & Rail~WE, 0x09310100)
+    add(Onewayroad~NS & Rail~WE,  0x09310100)
     add(Onewayroad~NS & L1Dtr~WE, 0x5d671200)
     add(Onewayroad~NS & L2Dtr~WE, 0x5d771200)
-    add(Onewayroad~NS & Str~WE, 0x5d342000)
+    add(Onewayroad~NS & Str~WE,   0x5d342000)
+    // -- OWR L1 --
+    add(L1Onewayroad~NS & Rail~WE,  0x5c011500)
+    add(L1Onewayroad~NS & L2Dtr~WE, 0x5d771210)
+    add(L1Onewayroad~NS & Str~WE,   0x5c011505)
     // -- Avenue --
-    add(Avenue~NS & Rail~WE, 0x04001500)
+    add(Avenue~NS & Rail~WE,  0x04001500)
     add(Avenue~NS & L1Dtr~WE, 0x5d671300)
     add(Avenue~NS & L2Dtr~WE, 0x5d771300)
-    add(Avenue~NS & Str~WE, 0x5d343000)
+    add(Avenue~NS & Str~WE,   0x5d343000)
     // -- Avenue L1 --
-    add(L1Avenue~NS & Rail~WE, 0x5c021500)
+    add(L1Avenue~NS & Rail~WE,  0x5c021500)
     add(L1Avenue~NS & L2Dtr~WE, 0x5d771300)
-    add(L1Avenue~NS & Str~WE, 0x5c021505)
+    add(L1Avenue~NS & Str~WE,   0x5c021505)
+    // -- Avenue L2 --
+    add(L2Avenue~NS & Rail~WE,  0x5c051500)
+    add(L2Avenue~NS & L1Dtr~WE, 0x5d671320)
+    add(L2Avenue~NS & Str~WE,   0x5c051505)
     // ----- OxD -----
+    // -- Street --
+    add(Street~WN & Rail~NS, 0x5f502600)
     // -- Road --
+    add(Road~WN & Rail~NS, 0x03020100)
+    add(Road~ES & L1Dtr~NS, 0x5d674100)
+    add(Road~ES & L2Dtr~NS, 0x5d774100)
+    // -- Road L1 --
+    add(L1Road~ES & Rail~WE, 0x5c007500)
+    add(L1Road~ES & L2Dtr~NS, 0x5d774110)
+    // -- Road L2 --
+    add(L2Road~ES & Rail~WE, 0x5c037500)
+    add(L2Road~ES & L1Dtr~NS, 0x5d67410a)
+    // -- OWR --
+    add(Onewayroad~WN & Rail~NS, 0x09320100)
+    // -- Avenue --
+    add(Avenue~ES & Rail~NS, 0x04004700)
+    add(Avenue~SharedDiagRight & Rail~NS, 0x04004600)
     // ----- DxO -----
     // -- Road --
     // ----- DxD -----
--- src/main/scala/module/RealRailwayRuleGenerator.scala
+++ src/main/scala/module/RealRailwayRuleGenerator.scala
@@ -8,7 +8,28 @@ import NetworkProperties._
 
 class RealRailwayRuleGenerator(val resolver: IdResolver) extends RuleGenerator {
   def start(): Unit = {
-    Rules += Str~WE | (Rail ~> Str)~WE & Road~NS
+    /*
+    Generate OxO rules by iteration over list of supported crossings
+    */
+    val RrwNetworks = List(L1Dtr, L2Dtr)
+    val CrossNetworks = List(Road, L1Road, L2Road)
+    
+    for (main <- RrwNetworks; base <- main.base) {
+      Rules += main~WE | (base ~> main)~WE      // ortho
+      for (minor <- CrossNetworks if minor.height != main.height) {
+        Rules += main~WE | (base ~> main)~WE & minor~NS // OxO
+        Rules += main~WE & minor~NS | (base ~> main)~WE // OxO continue
+        for(minor2 <- CrossNetworks if minor2.height != main.height) {
+          Rules += main~WE & minor~NS | (base ~> main)~WE & minor2~NS // OxO | OxO adjacencies
+        }
+        
+        Rules += main~WE | (base ~> main)~WE & minor~SE // OxD
+        Rules += main~EW & minor~SW | (base ~> main)~WE // OxD continue
+        for(minor2 <- CrossNetworks if minor2.height != main.height) {
+          Rules += main~WE & minor~WN | (base ~> main)~WE & minor2~NS // OxD | OxO adjacencies
+        }
+      }
+    }
     createRules()
   }
 }
