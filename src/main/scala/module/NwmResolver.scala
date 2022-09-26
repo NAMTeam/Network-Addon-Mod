fac0b8cadf1989f7f8a9774446226224ad698ac8
memo <memo33@users.noreply.github.com>
2022-09-26 17:45:24 +0200

    update NWM IID scheme to current standard

--- src/main/scala/module/NwmResolver.scala
+++ src/main/scala/module/NwmResolver.scala
@@ -10,47 +10,63 @@ class NwmResolver extends IdResolver with NwmSingleSegResolver with DoubleSegRes
 
   val isSingleTileNwm = Set(Tla3, Ave2, Ard3, Owr1, Owr3, Nrd4)
 
-  val nwmRangeId = {
-    val tmp = NwmNetworks.filter(n => n != Ave6m && n != Tla7m).toSeq.zipWithIndex map { case (n, i) =>
-      (n, 0x51700000 + i * 0x10000)
-    }
-    (tmp :+ ((Tla7m, 0x517B0080)) :+ ((Ave6m, 0x517C0080))).toMap
-  }.lift
+  val nwmRangeId = Map(
+    Tla3          -> 0x51000000,
+    Ave2          -> 0x51010000,
+    Ard3          -> 0x51020000,
+    Owr1          -> 0x51030000,
+    Owr3          -> 0x51040000,
+    Nrd4          -> 0x51050000,
+
+    Tla5          -> 0x51100000,
+    Owr4          -> 0x51110000,
+    Owr5          -> 0x51120000,
+    Rd4           -> 0x51130000,
+    Rd6           -> 0x51140000,
+
+    Ave6          -> 0x51200000,
+    Tla7m         -> 0x51200080,
+    Ave8          -> 0x51210000,
+    Ave6m         -> 0x51210080).lift
 
   val nwmPieceId = Map(
-    Street        -> 0x1000,
-    Road          -> 0x1100,
-    Onewayroad    -> 0x1200,
-    Avenue        -> 0x1300,
-    Highway       -> 0x1420,
-    Groundhighway -> 0x1400,
-    Rail          -> 0x1500,
-    Str           -> 0x1505,
-//    Ttr           -> 0x1600,
-//    Qtr           -> 0x1605,
-    Lightrail     -> 0x1720,
-    Glr1          -> 0x1700,
-    Glr2          -> 0x1800,
-    Glr3          -> 0x1705,
-    Glr4          -> 0x1805,
-    Monorail      -> 0x1920,
-    Hsr           -> 0x1905,
-    L2Hsr         -> 0x1925,
-    Tla3          -> 0x1A00,
-    Ave2          -> 0x1B00,
-    Ard3          -> 0x1C00,
-    Nrd4          -> 0x1D00,
-    Owr1          -> 0x1E00,
-    Owr3          -> 0x1F00,
-    Tla5          -> 0x2000,
-    Rd4           -> 0x2100,
-    Rd6           -> 0x2200,
-    Owr4          -> 0x2300,
-    Owr5          -> 0x2400,
-    Ave6          -> 0x2500,
-    Tla7m         -> 0x2600,
-    Ave8          -> 0x2700,
-    Ave6m         -> 0x2800).lift
+    Street        -> 0x0000,
+    Road          -> 0x0100,
+    Onewayroad    -> 0x0200,
+    Avenue        -> 0x0300,
+    Highway       -> 0x0420,
+    Groundhighway -> 0x0400,
+    Rail          -> 0x0500,
+    Lightrail     -> 0x0600,
+    Monorail      -> 0x0700,
+    Glr1          -> 0x0800,
+    Glr2          -> 0x0900,
+
+    Str           -> 0x0F00,
+
+    Tla3          -> 0x1000,
+    Ave2          -> 0x1100,
+    Ard3          -> 0x1200,
+    Owr1          -> 0x1300,
+    Owr3          -> 0x1400,
+    Nrd4          -> 0x1500,
+
+    L2Hsr         -> 0x1700,
+
+    Tla5          -> 0x1800,
+    Owr4          -> 0x1900,
+    Owr5          -> 0x1A00,
+    Rd4           -> 0x1B00,
+    Rd6           -> 0x1C00,
+
+    Ave6          -> 0x1D00,
+    Tla7m         -> 0x1D0A,
+    Ave8          -> 0x1E00,
+    Ave6m         -> 0x1E0A).lift
+    // currently not defined:
+    // Glr3          -> 0x....,
+    // Glr4          -> 0x....,
+    // Hsr           -> 0x....,
 
   /** is defined for all tiles that do not contain RHW, but NWM */
   def isDefinedAt(t: Tile): Boolean = !t.segs.exists(_.network.isRhw) && t.segs.exists(_.network.isNwm)
@@ -66,9 +82,15 @@ class NwmResolver extends IdResolver with NwmSingleSegResolver with DoubleSegRes
       assert(maj.network.isNwm)
       doubleProps.get(maj.flags, min.flags) match {
         case Some(prop) =>
-          var id = nwmRangeId(maj.network).get + nwmPieceId(min.network).get + prop.orthDiagOffset
+          val pieceOffset = prop.orthDiagOffset match {
+            case 0x0000 => 0x1000  // OxO
+            case 0x3000 => 0x5000  // OxD
+            case 0x6000 => 0x7000  // DxO
+            case 0x9000 => 0x9000  // DxD
+          }
+          var id = nwmRangeId(maj.network).get + nwmPieceId(min.network).get + pieceOffset
           if (prop.majorSegReversed)
-            id += (if (isSingleTileNwm(maj.network)) 0x80 else 0x40)
+            id += 0x80  // (if (isSingleTileNwm(maj.network)) 0x80 else 0x40)
           if (prop.minorSegReversed)
             id += (/*if (maj.network.height == 0 && min.network.height == 0) 0x09 else*/ 0x05)
           if (prop.majKind == Flag.Kind.LeftHeaded || prop.minKind == Flag.Kind.LeftHeaded ||
@@ -76,7 +98,7 @@ class NwmResolver extends IdResolver with NwmSingleSegResolver with DoubleSegRes
               tile.symmetries.exists(_.flipped)) // <-- does not have right-headed ID
             IdTile(id, prop.rf, leftHeadedMappedRepr)
           else if (prop.majKind == Flag.Kind.RightHeaded || prop.minKind == Flag.Kind.RightHeaded)
-            IdTile(id + 1, prop.rf, rightHeadedMappedRepr) // TODO find suitable ID
+            IdTile(id + 0x20000000, prop.rf, rightHeadedMappedRepr) // TODO find suitable ID
           else
             IdTile(id, prop.rf)
         case None => //??? // TODO T intersections etc. still missing
@@ -85,7 +107,7 @@ class NwmResolver extends IdResolver with NwmSingleSegResolver with DoubleSegRes
     } else if (tile.segs.size == 1) {
       resolveNwmSegment(tile.segs.head)
     } else {
-      ??? // TODO
+      throw new NotImplementedError(tile.toString) // ??? // TODO
     }
   }
 }
