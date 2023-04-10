5dbda1f9f434724e3456146f5d97475aab590a0d
TarkusSC4 <tarkustarkus@hotmail.com>
2023-04-10 05:19:17 -0700

    Adds support for OWR crossings, stubs, and some initial non-standard self-intersections.

--- src/main/scala/module/SamRuleGenerator.scala
+++ src/main/scala/module/SamRuleGenerator.scala
@@ -14,7 +14,7 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
     */
     val SamNetworks = List(Sam2, Sam3, Sam4, Sam5, Sam6, Sam7, Sam8, Sam9, Sam10, Sam11)
 
-    val CrossNetworks = List(Street, Road/*, Avenue, Onewayroad*/,
+    val CrossNetworks = List(Street, Road/*, Avenue*/, Onewayroad,
     Rail, Lightrail, Monorail/*,Glr1, Glr2, Str, Dirtroad, Rhw3, Mis, Rhw4, Rhw6s, Rhw8sm, Rhw8s, Rhw10s, Rhw12s, Rhw6cm,
     Rhw6c, Rhw8c, L1Rhw2, L1Rhw3, L1Mis, L1Rhw4, L1Rhw6s, L1Rhw8sm, L1Rhw8s, L1Rhw10s, L1Rhw12s, L1Rhw6cm,
     L1Rhw6c, L1Rhw8c, L2Rhw2, L2Rhw3, L2Mis, L2Rhw4, L2Rhw6s, L2Rhw8sm, L2Rhw8s, L2Rhw10s, L2Rhw12s, L2Rhw6cm,
@@ -25,9 +25,20 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
     for (main <- SamNetworks; base <- main.base) {
 
       Rules += main~WE | (base ~> main)~WE      // ortho
-      Rules += main~WE | base~CW | % | main~CW  // overrides end stub to orth Sam
+      Rules += main~WE | base~CW | % | main~CW  // ortho stub
       Rules += main~SE~ES | (base ~> main)~WN~NW   // diagonal
-
+	  Rules += main~SE~ES | (base ~> main)~(3,0,0,0)   // diagonal stub
+	  
+	  Rules += main~WE | (base ~> main)~(2,0,11,0)	// orth to orth-diag bottom
+	  Rules += main~(2,0,11,0) | (base ~> main)~(11,3,0,0) // orth-diag bottom to orth-diag top
+	  Rules += main~(0,0,1,13) | (base ~> main)~NW~WN // orth-diag top to diag
+	  Rules += main~(0,0,1,13) | (base ~> main)~(3,0,0,0) // orth-diag top to diag stub
+	 
+	  Rules += main~SE~ES | (base ~> main)~(1,13,0,0) //diag to orth-diag top
+	  Rules += main~(0,0,11,3) | (base ~> main)~(11,0,2,0) //orth-diag top to orth-diag bottom
+	  Rules += main~(11,0,2,0) | (base ~> main)~WE //orth-diag bottom to orth
+	  Rules += main~(11,0,2,0) | (base ~> main)~WC //orth-diag bottom to orth stub
+	  
       for (minor <- CrossNetworks) {
         createAdjacentIntersections(main, base, minor)
 
@@ -46,18 +57,18 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           // DxD
           Rules += main~ES | (base ~> main)~NW & minor~EN       // DxD
           Rules += main~SE & minor~WS | (base ~> main)~NW       // DxD continue
-		  if(minor == Street || minor == Road ||minor == Onewayroad /*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
+		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
 				//OxO T (main thru)
-				Rules += main~WE | (base ~> main)~WE & minor~NC
-				Rules += main~WE | (base ~> main)~WE & minor~CS
+				Rules += main~WE | (base ~> main)~WE & minor~NC~CN
+				Rules += main~WE | (base ~> main)~WE & minor~CS~SC
 				//continue
-				Rules += main~WE & minor~NC | (base ~> main)~WE
-				Rules += main~WE & minor~CS | (base ~> main)~WE
+				Rules += main~WE & minor~NC~CN | (base ~> main)~WE
+				Rules += main~WE & minor~CS~SC | (base ~> main)~WE
 				//continue stub
-				Rules += main~WE & minor~NC | (base ~> main)~WC
-				Rules += main~WE & minor~CS | (base ~> main)~WC
+				Rules += main~WE & minor~NC~CN | (base ~> main)~WC
+				Rules += main~WE & minor~CS~SC | (base ~> main)~WC
 			}
-		  if(minor == Street || minor == Road ||minor == Onewayroad /*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
+		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
 				//OxO T (main end)
 				Rules += main~WC | (base ~> main)~WC & minor~NS
 				Rules += main~WC | (base ~> main)~WC & minor~NS
@@ -69,24 +80,55 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           Rules += main~WE | (base ~> main)~WE & minor~NS             // OxO
           Rules += main~WE & minor~NS | (base ~> main)~WE & minor~SN      // OxO far side
           Rules += main~WE & minor~SN | (base ~> main)~WE             // OxO continue
+          Rules += main~WE & minor~SN | (base ~> main)~WC             // OxO stub
           // OxD
           Rules += main~WE | (base ~> main)~WE & minor~ES             // OxD start
           Rules += main~WE & minor~ES | (base ~> main)~WE & minor~SharedDiagRight                   // OxD middle
           Rules += main~WE & minor~SharedDiagRight | (base ~> main)~WE & minor~WN       // OxD end
           Rules += main~WE & minor~WN | (base ~> main)~WE             // OxD continue
+          Rules += main~WE & minor~WN | (base ~> main)~WC             // OxD continue stub
+
           // DxO
           Rules += main~ES | (base ~> main)~NW & minor~NS                 // DxO start
           Rules += main~EN & minor~EW | (base ~> main)~SW & minor~EW      // DxO middle 1
           Rules += main~ES & minor~NS | (base ~> main)~NW & minor~SN      // DxO middle 2
           Rules += main~EN & minor~WE | (base ~> main)~SW & minor~WE      // DxO end
           Rules += main~ES & minor~SN | (base ~> main)~NW                 // DxO continue
+          Rules += main~ES & minor~SN | (base ~> main)~(3,0,0,0)          // DxO continue stub
+
           // DxD
           Rules += main~ES | (base ~> main)~NW & minor~NE                                           // DxD start
           Rules += main~EN & minor~ES | (base ~> main)~SW & minor~SharedDiagRight                   // DxD middle
           Rules += main~ES & minor~SharedDiagLeft | (base ~> main)~NW & minor~SW                    // DxD end
           Rules += main~ES & minor~SW | (base ~> main)~NW           // DxD continue
+          Rules += main~ES & minor~SW | (base ~> main)~(3,0,0,0)    // DxD continue stub
         }
 
+		// non-standard self-intersections
+		//from ortho
+		Rules += main~WE | (base ~> main)~(2,11,2,0)
+		Rules += main~WE | (base ~> main)~(2,13,2,0)
+		//continuation
+		Rules += main~(2,11,2,0) | (base ~> main)~WE
+		Rules += main~(2,13,2,0) | (base ~> main)~WE
+		Rules += main~(2,11,2,0) | (base ~> main)~WC
+		Rules += main~(2,13,2,0) | (base ~> main)~WC
+		
+		//from ortho
+		Rules += main~WE | (base ~> main)~(2,11,2,2)
+		Rules += main~WE | (base ~> main)~(2,13,2,2)
+		// Rules += main~WE | (base ~> main)~(2,2,2,11)
+		// Rules += main~WE | (base ~> main)~(2,2,2,13)
+		//continuation
+		Rules += main~(2,11,2,2) | (base ~> main)~WE
+		Rules += main~(2,13,2,2) | (base ~> main)~WE
+		// Rules += main~(2,2,2,11) | (base ~> main)~WE
+		// Rules += main~(2,2,2,13) | (base ~> main)~WE
+		Rules += main~(2,11,2,2) | (base ~> main)~WC
+		Rules += main~(2,13,2,2) | (base ~> main)~WC
+		// Rules += main~(2,2,2,11) | (base ~> main)~WC
+		// Rules += main~(2,2,2,13) | (base ~> main)~WC	
+		
         /*
         if (hasRightShoulder(minor)) {
           Rules += main~WE | (base ~> main)~WE & minor~NS       // OxO
