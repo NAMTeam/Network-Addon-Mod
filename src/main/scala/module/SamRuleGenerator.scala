b4c753786b9827455a5594e686571040fbd80082
TarkusSC4 <tarkustarkus@hotmail.com>
2023-04-11 06:50:44 -0700

    Revises implementation of SAM x SAM crossings and base SAM setups in SamRuleGenerator.

--- src/main/scala/module/SamRuleGenerator.scala
+++ src/main/scala/module/SamRuleGenerator.scala
@@ -1,7 +1,7 @@
 package metarules.module
 
 import metarules.meta._
-import Network._, Flags._, Flag._, RotFlip._, Implicits._
+import Network._, Flags._, Flag._, RotFlip._, Implicits._, group.SymGroup._
 import scala.collection.mutable.Buffer
 import NetworkProperties._
 
@@ -14,7 +14,7 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
     */
     val SamNetworks = List(Sam2, Sam3, Sam4, Sam5, Sam6, Sam7, Sam8, Sam9, Sam10, Sam11)
 
-    val CrossNetworks = List(Street, Road, Avenue, Onewayroad,
+    val CrossNetworks = List(Road, Avenue, Onewayroad,
     Rail, Lightrail, Monorail, Glr1, Glr2/*, Str, Dirtroad, Rhw3, Mis, Rhw4, Rhw6s, Rhw8sm, Rhw8s, Rhw10s, Rhw12s, Rhw6cm,
     Rhw6c, Rhw8c, L1Rhw2, L1Rhw3, L1Mis, L1Rhw4, L1Rhw6s, L1Rhw8sm, L1Rhw8s, L1Rhw10s, L1Rhw12s, L1Rhw6cm,
     L1Rhw6c, L1Rhw8c, L2Rhw2, L2Rhw3, L2Mis, L2Rhw4, L2Rhw6s, L2Rhw8sm, L2Rhw8s, L2Rhw10s, L2Rhw12s, L2Rhw6cm,
@@ -24,20 +24,85 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
 
     for (main <- SamNetworks; base <- main.base) {
 
-      Rules += main~WE | (base ~> main)~WE      // ortho
+      Rules += main~WE | (base ~> main)~WE~EW      // ortho
       Rules += main~WE | base~CW | % | main~CW  // ortho stub
       Rules += main~SE~ES | (base ~> main)~WN~NW   // diagonal
       Rules += main~SE~ES | (base ~> main)~(3,0,0,0)   // diagonal stub
-      
+      Rules += main~NE~EN | (base ~> main)~(1,0,0,0)   // diagonal stub
+     
       Rules += main~WE | (base ~> main)~(2,0,11,0)	// orth to orth-diag bottom
       Rules += main~(2,0,11,0) | (base ~> main)~(11,3,0,0) // orth-diag bottom to orth-diag top
+      Rules += main~(2,0,11,0) | (base ~> main)~(1,3,0,0) // orth-diag bottom to orth-diag top fix 1
+      Rules += main~(2,0,2,0) | (base ~> main)~(11,3,0,0) // orth-diag bottom to orth-diag top fix 2
+      Rules += main~(2,0,2,0) | (base ~> main)~(1,3,0,0) // orth-diag bottom to orth-diag top fix 3
       Rules += main~(0,0,1,13) | (base ~> main)~NW~WN // orth-diag top to diag
       Rules += main~(0,0,1,13) | (base ~> main)~(3,0,0,0) // orth-diag top to diag stub
-    
+      Rules += main~(0,0,1,13) | (base ~> main)~(3,0,0,0) // orth-diag top to diag stub
+   
       Rules += main~SE~ES | (base ~> main)~(1,13,0,0) //diag to orth-diag top
       Rules += main~(0,0,11,3) | (base ~> main)~(11,0,2,0) //orth-diag top to orth-diag bottom
+      Rules += main~(0,0,1,3) | (base ~> main)~(11,0,2,0) //orth-diag top to orth-diag bottom fix 1
+      Rules += main~(0,0,11,3) | (base ~> main)~(2,0,2,0) //orth-diag top to orth-diag bottom fix 2
+      Rules += main~(0,0,1,3) | (base ~> main)~(2,0,2,0) //orth-diag top to orth-diag bottom fix 3
       Rules += main~(11,0,2,0) | (base ~> main)~WE //orth-diag bottom to orth
       Rules += main~(11,0,2,0) | (base ~> main)~WC //orth-diag bottom to orth stub
+	  
+	  // Rules += main~WE | (base ~> main)~(2,2,2,2) //alternate attempt at OxO +
+	  // Rules += main~(2,2,2,2) | (base ~> main)~WE //alternate attempt at OxO continue
+	  // Rules += main~(2,2,2,2) | (base ~> main)~WC //alternate attempt at OxO continue stub
+	  
+	  // Rules += main~WE | (base ~> main)~WE & (base ~> main)~NS  	// OxO
+      // Rules += main~WE & main~NS~SN | (base ~> main)~WE          	// OxO continue
+      // Rules += main~WE & main~NS~SN | (base ~> main)~WC          	// OxO continue stub
+	
+		Rules += main~WE    | (base ~> main)~WE & (base ~> main)~NS
+		Rules += main~WE & main~SN    | (base ~> main)~WE
+		  
+      Rules += main~WE | (base ~> main)~WE & (base ~> main)~NC		// OxO T Thru-Side
+      Rules += main~WE | (base ~> main)~WC & (base ~> main)~NS		// OxO T End-Side
+      Rules += main~WE & main~NC | (base ~> main)~WE          		// OxO T Thru-Side continue
+      Rules += main~WE & main~CS | (base ~> main)~WE          		// OxO T Thru-Side continue
+      Rules += main~WE & main~NC | (base ~> main)~WC          		// OxO T Thru-Side continue stub
+      Rules += main~WE & main~CS | (base ~> main)~WC          		// OxO T Thru-Side continue stub
+		  
+	  Rules += main~WE | (base ~> main)~WE & (base ~> main)~ES 		// OxD from orth
+      Rules += main~WE & main~ES~SE | (base ~> main)~WE          	// OxD orth continue
+      Rules += main~WE & main~ES~SE | (base ~> main)~WC          	// OxD orth continue stub
+		  
+	  Rules += main~ES | (base ~> main)~NW & (base ~> main)~NS      // DxO
+	  Rules += main~ES & main~NS | (base ~> main)~NW       			// DxO diag continue
+	  Rules += main~ES & main~NS | (base ~> main)~(3,0,0,0)       	// DxO diag stub continue
+		  
+      Rules += main~ES | (base ~> main)~NW & (base ~> main)~EN      // DxD
+      Rules += main~SE & main~WS | (base ~> main)~NW       			// DxD continue
+      Rules += main~SE & main~WS | (base ~> main)~(3,0,0,0)       	// DxD continue stub
+	  
+	  // non-standard self-intersections
+      //from ortho
+      Rules += main~WE | (base ~> main)~(2,11,2,0)
+      Rules += main~WE | (base ~> main)~(2,13,2,0)
+      //continuation
+      Rules += main~(2,11,2,0) | (base ~> main)~WE
+      Rules += main~(2,13,2,0) | (base ~> main)~WE
+      Rules += main~(2,11,2,0) | (base ~> main)~WC
+      Rules += main~(2,13,2,0) | (base ~> main)~WC
+      
+      //from ortho
+      Rules += main~WE | (base ~> main)~(2,11,2,2)
+      Rules += main~WE | (base ~> main)~(2,13,2,2)
+      // Rules += main~WE | (base ~> main)~(2,2,2,11)
+      // Rules += main~WE | (base ~> main)~(2,2,2,13)
+      //continuation
+      Rules += main~(2,11,2,2) | (base ~> main)~WE
+      Rules += main~(2,13,2,2) | (base ~> main)~WE
+      // Rules += main~(2,2,2,11) | (base ~> main)~WE
+      // Rules += main~(2,2,2,13) | (base ~> main)~WE
+      Rules += main~(2,11,2,2) | (base ~> main)~WC
+      Rules += main~(2,13,2,2) | (base ~> main)~WC
+      // Rules += main~(2,2,2,11) | (base ~> main)~WC
+      // Rules += main~(2,2,2,13) | (base ~> main)~WC	
+
+
 	  
       for (minor <- CrossNetworks) {
         
@@ -45,13 +110,11 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           createAdjacentIntersections(main, base, minor)
         }
       
-        if (minor == Street) {
-          Rules += main~WE | (base ~> main)~WE & (minor ~> main)~NS~SN          // OxO
-          Rules += main~WE & main~NS~SN | (base ~> main)~WE          // OxO continue
-          Rules += main~WE & main~NS~SN | (base ~> main)~WC          // OxO continue stub
+        if (base == Street) {
+		  
         }
 
-        if (isSingleTile(minor) && minor !=Street) {
+        if (isSingleTile(minor)) {
           // OxO
           Rules += main~WE | (base ~> main)~WE & minor~NS~SN          // OxO
           Rules += main~WE & minor~NS~SN | (base ~> main)~WE          // OxO continue
@@ -70,7 +133,7 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           }
         }
 
-        if (minor == Street || minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
+        if (minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
           //OxO T (main thru)
           Rules += main~WE | (base ~> main)~WE & minor~NC~CN
           Rules += main~WE | (base ~> main)~WE & minor~CS~SC
@@ -82,10 +145,9 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           Rules += main~WE & minor~CS~SC | (base ~> main)~WC
         }
 
-        if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
+        if(minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
           //OxO T (main end)
-          Rules += main~WC | (base ~> main)~WC & minor~NS
-          Rules += main~WC | (base ~> main)~WC & minor~NS
+          Rules += main~WE | (base ~> main)~WC & minor~NS
         }
 
         if (minor.typ == AvenueLike) {
@@ -115,31 +177,6 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           Rules += main~ES & minor~SW | (base ~> main)~(3,0,0,0)                   // DxD continue stub
         }
       }
-
-      // non-standard self-intersections
-      //from ortho
-      Rules += main~WE | (base ~> main)~(2,11,2,0)
-      Rules += main~WE | (base ~> main)~(2,13,2,0)
-      //continuation
-      Rules += main~(2,11,2,0) | (base ~> main)~WE
-      Rules += main~(2,13,2,0) | (base ~> main)~WE
-      Rules += main~(2,11,2,0) | (base ~> main)~WC
-      Rules += main~(2,13,2,0) | (base ~> main)~WC
-      
-      //from ortho
-      Rules += main~WE | (base ~> main)~(2,11,2,2)
-      Rules += main~WE | (base ~> main)~(2,13,2,2)
-      // Rules += main~WE | (base ~> main)~(2,2,2,11)
-      // Rules += main~WE | (base ~> main)~(2,2,2,13)
-      //continuation
-      Rules += main~(2,11,2,2) | (base ~> main)~WE
-      Rules += main~(2,13,2,2) | (base ~> main)~WE
-      // Rules += main~(2,2,2,11) | (base ~> main)~WE
-      // Rules += main~(2,2,2,13) | (base ~> main)~WE
-      Rules += main~(2,11,2,2) | (base ~> main)~WC
-      Rules += main~(2,13,2,2) | (base ~> main)~WC
-      // Rules += main~(2,2,2,11) | (base ~> main)~WC
-      // Rules += main~(2,2,2,13) | (base ~> main)~WC	
 		
     }
     createRules()
