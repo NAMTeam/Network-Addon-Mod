3ee101242f83ae1f58dbd2c63503baa6d543de34
TarkusSC4 <tarkustarkus@hotmail.com>
2023-04-10 18:26:39 -0700

    Enables Avenue and GLR crossings for SAM MetaRULs.

--- src/main/scala/module/SamRuleGenerator.scala
+++ src/main/scala/module/SamRuleGenerator.scala
@@ -14,8 +14,8 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
     */
     val SamNetworks = List(Sam2, Sam3, Sam4, Sam5, Sam6, Sam7, Sam8, Sam9, Sam10, Sam11)
 
-    val CrossNetworks = List(Street, Road/*, Avenue*/, Onewayroad,
-    Rail, Lightrail, Monorail/*,Glr1, Glr2, Str, Dirtroad, Rhw3, Mis, Rhw4, Rhw6s, Rhw8sm, Rhw8s, Rhw10s, Rhw12s, Rhw6cm,
+    val CrossNetworks = List(Street, Road, Avenue, Onewayroad,
+    Rail, Lightrail, Monorail, Glr1, Glr2/*, Str, Dirtroad, Rhw3, Mis, Rhw4, Rhw6s, Rhw8sm, Rhw8s, Rhw10s, Rhw12s, Rhw6cm,
     Rhw6c, Rhw8c, L1Rhw2, L1Rhw3, L1Mis, L1Rhw4, L1Rhw6s, L1Rhw8sm, L1Rhw8s, L1Rhw10s, L1Rhw12s, L1Rhw6cm,
     L1Rhw6c, L1Rhw8c, L2Rhw2, L2Rhw3, L2Mis, L2Rhw4, L2Rhw6s, L2Rhw8sm, L2Rhw8s, L2Rhw10s, L2Rhw12s, L2Rhw6cm,
     L2Rhw6c, L2Rhw8c, L3Mis, L3Rhw4, L3Rhw6s, L4Mis, L4Rhw4, L4Rhw6s, Tla3, Ave2, Ard3, Owr1, Owr3, Nrd4, Tla5, Owr4, 
@@ -41,8 +41,15 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
 	  
       for (minor <- CrossNetworks) {
         createAdjacentIntersections(main, base, minor)
+		
+		if (minor == Street) {
+		  Rules += main~WE | (base ~> main)~WE & (minor ~> main)~NS~SN          // OxO
+          Rules += main~WE & main~NS~SN | (base ~> main)~WE          // OxO continue
+          Rules += main~WE & main~NS~SN | (base ~> main)~WC          // OxO continue stub
 
-        if (isSingleTile(minor)) {
+		}
+
+        if (isSingleTile(minor) && minor !=Street) {
           // OxO
           Rules += main~WE | (base ~> main)~WE & minor~NS~SN          // OxO
           Rules += main~WE & minor~NS~SN | (base ~> main)~WE          // OxO continue
@@ -57,7 +64,7 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           // DxD
           Rules += main~ES | (base ~> main)~NW & minor~EN       // DxD
           Rules += main~SE & minor~WS | (base ~> main)~NW       // DxD continue
-		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
+		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || minor == Tla3 || minor == Ave2 || minor == Ard3 || minor == Owr1 || minor == Nrd4 */) { 
 				//OxO T (main thru)
 				Rules += main~WE | (base ~> main)~WE & minor~NC~CN
 				Rules += main~WE | (base ~> main)~WE & minor~CS~SC
@@ -68,7 +75,7 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
 				Rules += main~WE & minor~NC~CN | (base ~> main)~WC
 				Rules += main~WE & minor~CS~SC | (base ~> main)~WC
 			}
-		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Avenue || minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
+		  if(minor == Street || minor == Road || minor == Onewayroad/*|| minor == Dirtroad || minor == Rhw3 || minor == Mis || minor == Rhw4 || isNwm(minor)*/) { 
 				//OxO T (main end)
 				Rules += main~WC | (base ~> main)~WC & minor~NS
 				Rules += main~WC | (base ~> main)~WC & minor~NS
@@ -161,9 +168,9 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
   }
 }
 
-// Compile individually with `sbt "runMain metarules.module.CompileOnslopeCode"`.
+// Compile individually with `sbt "runMain metarules.module.CompileSamCode"`.
 object CompileSamCode extends AbstractMain {
-  lazy val resolve: IdResolver = new MiscResolver orElse new SamResolver
+  lazy val resolve: IdResolver = new SamResolver orElse new MiscResolver orElse new NwmResolver
   lazy val generator: RuleGenerator = new SamRuleGenerator(RuleTransducer.Context(resolve))
   lazy val file = new java.io.File("target/SAM_MetaGenerated_MANAGED.txt")
 }
