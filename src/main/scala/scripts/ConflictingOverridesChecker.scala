5000afba48a1d40e14bc505162cedcc24b0b5211
memo <memo33@users.noreply.github.com>
2025-01-13 18:44:16 +0100

    add `--include-duplicates` option to identify duplicate (non-conflicting) RUL2 overrides

--- src/main/scala/scripts/ConflictingOverridesChecker.scala
+++ src/main/scala/scripts/ConflictingOverridesChecker.scala
@@ -25,14 +25,21 @@ import SanityChecker.{fileEndsWithNewline, linePatternIncludingNewlines}
   * The first rule in a pair of conflicts is never tagged, as it is the one that
   * will have an effect in the game.
   * Make sure to commit your changes before running this command.
+  *
+  * In order to also flag (non-conflicting) duplicates, use one of the following commands:
+  * {{{
+  * SBT_OPTS="-Xmx2G" sbt "conflictingOverridesCheck --include-duplicates"
+  * SBT_OPTS="-Xmx2G" sbt "conflictingOverridesCheck --include-duplicates --update"
+  * }}}
   */
 object ConflictingOverridesChecker {
 
   val tagBoth = "conflicting-override"
   val tagRhd = "conflicting-override_rhd"  // underscore (instead of hyphen) for use with regex word boundaries
   val tagLhd = "conflicting-override_lhd"
+  val tagDuplicate = "duplicate_override"
   val tagOf = Map[Driveside, String](RhdAndLhd -> tagBoth, Rhd -> tagRhd, Lhd -> tagLhd)
-  val allTags = Seq(tagBoth, tagRhd, tagLhd)
+  val allTags = Seq(tagBoth, tagRhd, tagLhd, tagDuplicate)
 
   /** Scans the Controller/RUL2 folder for conflicting duplicate RUL2 code.
     *
@@ -44,31 +51,31 @@ object ConflictingOverridesChecker {
     * In this case, the second line conflicts with the first one.
     */
   def main(args: Array[String]): Unit = {
-    if (args.isEmpty) {
-      val (numConflicts, _, _) = checkConflictingRul2(updateMode = false)
-      val msg = s"Found $numConflicts new conflicting RUL2 overrides."
+    val updateMode = args.contains("--update")
+    val duplicateMode = args.contains("--include-duplicates")
+    if (!updateMode) {
+      val (numConflicts, _, _, numDuplicates) = checkConflictingRul2(updateMode = updateMode, duplicateMode = duplicateMode)
+      val msg = s"Found $numConflicts new conflicting RUL2 overrides." + (if (duplicateMode) s" Found $numDuplicates duplicate RUL2 overrides." else "")
       if (numConflicts > 0) {
         LOGGER.severe(s"$msg Please avoid introducing new conflicts. Instead verify whether these overrides really have the intended effect and consider rewriting or removing them.")
         System.exit(1)
       } else {
         LOGGER.info(msg)
       }
-    } else if (args.sameElements(Seq("--update"))) {
-      val (numConflicts, removed, added) = checkConflictingRul2(updateMode = true)
-      val msg = s"Found $numConflicts conflicting RUL2 overrides (removed $removed, added $added)."
+    } else {
+      val (numConflicts, removed, added, numDuplicates) = checkConflictingRul2(updateMode = updateMode, duplicateMode = duplicateMode)
+      val msg = s"Found $numConflicts conflicting RUL2 overrides (removed $removed, added $added)." + (if (duplicateMode) s" Found $numDuplicates duplicate RUL2 overrides." else "")
       if (numConflicts > 0) {
         LOGGER.warning(msg)
       } else {
         LOGGER.info(msg)
       }
-    } else {
-      LOGGER.severe("wrong arguments")
-      System.exit(2)
     }
   }
 
-  def checkConflictingRul2(updateMode: Boolean): (Int, Int, Int) = {
+  def checkConflictingRul2(updateMode: Boolean, duplicateMode: Boolean): (Int, Int, Int, Int) = {
     var numConflicts = 0
+    var numDuplicates = 0
     var removed = 0
     var added = 0
     val rulesRhd = collection.mutable.Map.empty[EquivRule, Rule[IdTile]]
@@ -82,36 +89,35 @@ object ConflictingOverridesChecker {
     iterateRulFiles(directory).foreach { path =>
       val drivesideFile = drivesideOfFile(path)
 
-      // simultaneously builds the RUL2 cache and looks for conflicts
-      def findConflict(line: String): Option[(Rule[IdTile], Rule[IdTile], Driveside)] = {
+      // Simultaneously builds the RUL2 cache and looks for conflicts.
+      // Returns whether rule is duplicate (true) or conflicting (false).
+      def findConflict(line: String): Option[(Rule[IdTile], Rule[IdTile], Driveside, Boolean)] = {
         parseRuleWithRestrictedDriveside(line, drivesideFile) match {
           // only the first matching rule is loaded by the game, so we store only the first one read
           case Some((rule, Rhd)) =>
             val key = new EquivRule(rule)
             lookupRuleRhd.unapply(key) match {
-              case Some(rule2) => if (rulesHaveSameOutput(rule, rule2)) None else Some((rule, rule2, RhdAndLhd))
+              case Some(rule2) => Some((rule, rule2, RhdAndLhd, rulesHaveSameOutput(rule, rule2)))
               case None => rulesRhd.addOne(key, rule); None
             }
           case Some((rule, Lhd)) =>
             val key = new EquivRule(rule)
             lookupRuleLhd.unapply(key) match {
-              case Some(rule2) => if (rulesHaveSameOutput(rule, rule2)) None else Some((rule, rule2, RhdAndLhd))
+              case Some(rule2) => Some((rule, rule2, RhdAndLhd, rulesHaveSameOutput(rule, rule2)))
               case None => rulesLhd.addOne(key, rule); None
             }
           case Some((rule, RhdAndLhd)) =>
             val key = new EquivRule(rule)
             rulesShared.get(key) match {
-              case Some(rule2) => if (rulesHaveSameOutput(rule, rule2)) None else Some((rule, rule2, RhdAndLhd))
+              case Some(rule2) => Some((rule, rule2, RhdAndLhd, rulesHaveSameOutput(rule, rule2)))
               case None =>
-                if (!rulesRhd.contains(key) && !rulesLhd.contains(key)) {
-                  rulesShared.addOne(key, rule); None
-                } else {
-                  (rulesRhd.get(key).filterNot(rulesHaveSameOutput(rule, _)), rulesLhd.get(key).filterNot(rulesHaveSameOutput(rule, _))) match {
-                    case (None, None) => rulesShared.addOne(key, rule); None  // no conflict
-                    case (Some(rule2), None) => Some((rule, rule2, Rhd))  // conflict only with RHD rules
-                    case (None, Some(rule3)) => Some((rule, rule3, Lhd))  // conflict only with LHD rules
-                    case (Some(rule2), Some(rule3)) => Some((rule, rule2, RhdAndLhd)) // conflict with both
-                  }
+                (rulesRhd.get(key).map(r => r -> rulesHaveSameOutput(rule, r)), rulesLhd.get(key).map(r => r -> rulesHaveSameOutput(rule, r))) match {
+                  case (None, None) => rulesShared.addOne(key, rule); None  // no conflict
+                  case (Some((rule1, true)), None) => rulesShared.addOne(key, rule); Some((rule, rule1, Rhd, true))  // no conflict, only duplicate
+                  case (Some((rule1, false)), None) => Some((rule, rule1, Rhd, false))  // conflict only with RHD rules
+                  case (None, Some((rule2, true))) => rulesShared.addOne(key, rule); Some((rule, rule2, Lhd, true))  // no conflict, only duplicate
+                  case (None, Some((rule2, false))) => Some((rule, rule2, Lhd, false))  // conflict only with LHD rules
+                  case (Some((rule1, dup1)), Some((rule2, dup2))) => Some((rule, rule1, RhdAndLhd, dup1 && dup2)) // conflict with both
                 }
             }
           case None => None
@@ -120,12 +126,16 @@ object ConflictingOverridesChecker {
 
       var lineNumber = 0
       var badFile = false
-      def logConflict(x: Rule[IdTile], y: Rule[IdTile]): Unit = {
+      def logConflict(x: Rule[IdTile], y: Rule[IdTile], isDuplicate: Boolean): Unit = {
         if (!badFile) {
           LOGGER.info(s"==> $path")
           badFile = true
         }
-        LOGGER.warning(s"$lineNumber: ${x(0)},${x(1)}=${x(2)},${x(3)} conflicts with ${y(0)},${y(1)}=${y(2)},${y(3)}")
+        if (isDuplicate) {
+          LOGGER.warning(s"$lineNumber: ${x(0)},${x(1)}=${x(2)},${x(3)} is duplicate of ${y(0)},${y(1)}=${y(2)},${y(3)}")
+        } else {
+          LOGGER.warning(s"$lineNumber: ${x(0)},${x(1)}=${x(2)},${x(3)} conflicts with ${y(0)},${y(1)}=${y(2)},${y(3)}")
+        }
       }
 
       if (!updateMode) {
@@ -134,12 +144,12 @@ object ConflictingOverridesChecker {
             val line = scanner.nextLine()
             lineNumber += 1
             findConflict(line) match {
-              case Some((rule, rule2, _)) =>
+              case Some((rule, rule2, _, isDuplicate)) if duplicateMode || !isDuplicate =>
                 if (!line.contains(tagBoth)) {
-                  numConflicts += 1
-                  logConflict(rule, rule2)
+                  if (isDuplicate) numDuplicates += 1 else numConflicts += 1
+                  logConflict(rule, rule2, isDuplicate)
                 }
-              case None =>  // ignore
+              case _ =>  // ignore
             }
           }
         }
@@ -153,13 +163,18 @@ object ConflictingOverridesChecker {
           while (lineScanner.hasNext()) {
             val line = lineScanner.next()
             findConflict(line) match {
-              case Some((_, _, driveside)) =>
-                numConflicts += 1
-                if (!line.contains(tagBoth)) {
-                  added += 1
+              case Some((_, _, driveside, isDuplicate)) if duplicateMode || !isDuplicate =>
+                if (!isDuplicate) {
+                  numConflicts += 1
+                  if (!line.contains(tagBoth)) {
+                    added += 1
+                  }
+                  printer.println(replaceTags(line, add = Some(tagOf(driveside)), remove = allTags))
+                } else {  // no conflict, only duplicate
+                  numDuplicates += 1
+                  printer.println(replaceTags(line, add = Some(tagDuplicate), remove = allTags))
                 }
-                printer.println(replaceTags(line, add = Some(tagOf(driveside)), remove = allTags))
-              case None =>
+              case _ =>
                 if (line.contains(tagBoth)) {  // no conflict, so remove tag
                   removed += 1
                   printer.println(replaceTags(line, add = None, remove = allTags))
@@ -172,7 +187,7 @@ object ConflictingOverridesChecker {
         Files.move(tmpPath, path, java.nio.file.StandardCopyOption.REPLACE_EXISTING)
       }
     }
-    (numConflicts, removed, added)
+    (numConflicts, removed, added, numDuplicates)
   }
 
   def replaceTags(line: String, add: Option[String], remove: Seq[String]): String = {
