123c22326f98f5d7ce5223a72dff2b4a496f27aa
memo <memo33@users.noreply.github.com>
2025-07-28 21:25:41 +0200

    update to NAM DLL 1.1.1

--- lua/adv_nam_dll.lua
+++ lua/adv_nam_dll.lua
@@ -3,7 +3,7 @@
 -- This file defines advisor messages related to the NAM DLL.
 
 -- Note that this Lua file is not intended for distribution with the DLL itself, but for distribution with the NAM DBPF files to ensure compatibility between NAM and DLL.
-nam_dll_version_expected = "1.1.0"  -- needs to be updated whenever a new DLL version is released
+nam_dll_version_expected = "1.1.1"  -- needs to be updated whenever a new DLL version is released
 
 -- (When this script is first executed, the `nam_dll_version` is still `nil`, but it gets defined before the trigger conditions are evaluated.)
 function is_nam_dll_correct()
