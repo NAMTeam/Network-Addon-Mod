e27376c802316c033533d3b0e76ae64bd72bf047
Clarence Chen <clarencechenct@gmail.com>
2025-10-10 01:35:58 -0700

    Update new DxD thru int metarules to avoid base street thru int far tile

--- src/main/scala/module/SamRuleGenerator.scala
+++ src/main/scala/module/SamRuleGenerator.scala
@@ -802,16 +802,18 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           Rules += sam~NE & minor~CS | (Street ~> sam)~SW
 
           // DxD T (sam thru)
-          Rules += sam~ES | (Street ~> sam)~NW & minor~CNE  // DxD Thru T 1
-          // Rules += sam~ES | (Street ~> sam)~NW & minor~CEN  // DxD Thru T 2
-          // continue
-          Rules += sam~ES & minor~CEN | (Street ~> sam)~NW  // Temp Diagonal Continuation off other side of DxD T-Thru
-          // Rules += sam~ES & minor~CEN | (Street ~> sam)~NW & minor~CSW // DxD Thru T 1-2
-          // Rules += sam~NE & minor~CSE | (Street ~> sam)~WS & minor~CWN // DxD Thru T 2-1
+          Rules += sam~ES | (Street ~> sam)~NW & minor~CNE      // DxD Thru T 1
+          // Rules += sam~ES | (Street ~> sam)~NW & minor~CEN   // Eventual DxD Thru T 2 (pending base T-thru tile)
 
-          // Rules += sam~ES & minor~EN | sam~NW & minor~CSW | sam~ES & minor~CEN | % // DxD T 2-1 Alt (Truncate Stub)
-          // Rules += sam~ES & minor~CEN | Street~NW | % | sam~NW & minor~CSW // DxD Thru T 1-2 Alt (Propagate Minor)
-          // Rules += sam~ES & minor~CEN | sam~NW | % | sam~NW & minor~CSW // DxD Thru T 1-2 Alt (Propagate Minor)
+          // continue
+          Rules += sam~ES & minor~CEN | Street~NW | % | sam~NW & minor~CSW  // DxD Thru T 1-2 Alt
+          Rules += sam~ES & minor~CEN | sam~NW | % | sam~NW & minor~CSW     // DxD Thru T 1-2 Alt
+          Rules += sam~NE & minor~CSE | (Street ~> sam)~WS & minor~CWN      // DxD Thru T 2-1
+
+          // truncate minor stub on tile 1
+          Rules += sam~ES & minor~EN | Street~NW | sam~ES & minor~CEN | sam~NW & minor~CSW  // DxD T 2-1 Alt
+          Rules += sam~ES & minor~EN | sam~NW | sam~ES & minor~CEN | sam~NW & minor~CSW     // DxD T 2-1 Alt
+          Rules += sam~ES & minor~EN | sam~NW & minor~CSW | sam~ES & minor~CEN | %          // DxD T 2-1 Alt
         }
       }
 
