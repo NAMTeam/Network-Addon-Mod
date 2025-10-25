e2ae693c231b5dd2a1b20caafee930146e9001ca
Clarence Chen <clarencechenct@gmail.com>
2025-10-25 14:46:11 -0700

    Add rule to convert base Street short T intersections to SAM long T

--- src/main/scala/module/SamRuleGenerator.scala
+++ src/main/scala/module/SamRuleGenerator.scala
@@ -804,6 +804,9 @@ class SamRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerato
           // DxD T (sam thru)
           Rules += sam~ES | (Street ~> sam)~NW & minor~CNE      // DxD Thru T 1
           // Rules += sam~ES | (Street ~> sam)~NW & minor~CEN   // Eventual DxD Thru T 2 (pending base T-thru tile)
+          Rules += sam~ES | (Street ~> sam)~NW & minor~CWS      // Temp DxD Thru T 2 (works with DxD Thru T 1-2 Alt)
+
+          // Rules += sam~ES & minor~CEN | (Street ~> sam)~NW & minor~CSW   // Eventual DxD Thru T 1-2 (pending base T-thru tile)
           Rules += sam~ES & minor~CEN | Street~NW | % | sam~NW & minor~CSW  // DxD Thru T 1-2 Alt
           Rules += sam~ES & minor~CEN | sam~NW | % | sam~NW & minor~CSW     // DxD Thru T 1-2 Alt
           Rules += sam~NE & minor~CSE | (Street ~> sam)~WS & minor~CWN      // DxD Thru T 2-1
