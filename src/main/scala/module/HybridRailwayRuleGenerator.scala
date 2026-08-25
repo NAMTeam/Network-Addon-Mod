5e09a2a055c1e73a0b89e370b82effd3784d6c79
eggman121 <eggman121@users.noreply.github.com>
2026-08-25 19:41:44 +1000

    Syntax help on defining Metarul Crosslink code
    This is an ask for help on the HRW Crosslink metarul code. Any pointers would be appreciated. Thanks

--- src/main/scala/module/HybridRailwayRuleGenerator.scala
+++ src/main/scala/module/HybridRailwayRuleGenerator.scala
@@ -4,31 +4,19 @@ import io.github.memo33.metarules.meta._, syntax._, Network._, Flags._, RotFlip.
 import scala.collection.mutable.Buffer
 import NetworkProperties._
 
-
 class HybridRailwayRuleGenerator(var context: RuleTransducer.Context) extends RuleGenerator with Adjacencies with Stability {
 
+  val HrwNetworks = List(L1Hrw, L2Hrw)
+
   def start(): Unit = {
-    /*
-    Generate OxO rules by iteration over list of supported crossings
-    */
-      val HrwNetworks = List(L1Hrw, L2Hrw)
-
-    val CrossNetworks = List(Street, Road, Onewayroad
-      )
-
     for (main <- HrwNetworks; base <- main.base) {
-      // base
-      Rules += main~WE | (base ~> main)~WE        // ortho continue
-      Rules += main~WE | base~CW | % | main~WE    // ortho continue stub convert
-    
-      Rules += main~ES | (base ~> main)~NW        // diag continue
-      Rules += main~ES | base~CNW | % | main~NW   // diag stub convert
-       
-      //Crossings OxO
-      for (minor <- CrossNetworks if minor.height != main.height) {
-      Rules += main~WE & minor~NS | (base ~> main)~WE              // OxO continue
+      Rules += main~WE    | (base ~> main)~WE      // ortho
+      Rules += main~WE    | (base ~> main)~WC      // ortho stub
+      
+      // crossings (O×O, O×D, D×O, D×D)
+      for (minor <- CrossingGenerator.crossingNetworksOf(main)) {
       }
     }
-    createRules()
   }
+  createRules()
 }
